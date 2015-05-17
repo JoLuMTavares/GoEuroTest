b45ab6576a07e97f09e6a18100f3a47da241417c
JoLuMTavares <joaoluism.tavares@gmail.com>
2015-05-17 21:35:49 +0200

    Create StadtInfo.java

---
+++ StadtInfo.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package goeurotest;
+
+/**
+ *
+ * @author User
+ */
+public class StadtInfo {
+    
+    // The parameters
+    private int _id;
+    private String name;
+    private String type;
+    private float latitude;
+    private float longitude;
+    
+    public StadtInfo(int _id, String name,
+            String type, float latitude, float longitude) {
+        
+        this._id = _id;
+        this.name = name;
+        this.type = type;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    
+    }
+    
+    
+    // Returning the _id
+    public int getID() {
+        return this._id;
+    }
+    
+    // Returning the name
+    public String getName() {
+        return this.name;
+    }
+    
+    // Returning the type
+    public String getType() {
+        return this.type;
+    }
+            
+    // Returning the latitude
+    public float getLat() {
+        return this.latitude;
+    }
+    
+    // Returning the longitude
+    public float getLongit() {
+        return this.longitude;
+    }
+}
